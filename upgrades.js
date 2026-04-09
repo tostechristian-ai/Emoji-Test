e871dc2ddeb143de0a341fab095ed0e9fd051d5e
tostechristian-ai <tostechristian@gmail.com>
2026-04-08 22:45:48 -0400

    Create upgrades.js for upgrade system

---
+++ upgrades.js
@@ -0,0 +1,25 @@
+const UPGRADE_BORDER_COLORS = ['#FF5733', '#33FF57', '#3357FF'];
+const UPGRADE_OPTIONS = ['Speed', 'Power', 'Defense'];
+const PERMANENT_UPGRADES = ['Double Jump', 'Extra Life'];
+const ALWAYS_AVAILABLE_PICKUPS = ['Health Potion', 'Mana Potion'];
+const UNLOCKABLE_PICKUPS = ['Invisibility Cloak', 'Shield'];
+
+function levelUp() {
+    // Code to level up the player
+}
+
+function showUpgradeMenu() {
+    // Code to show the upgrade menu
+}
+
+function applyUpgrade(upgrade) {
+    // Code to apply the selected upgrade
+}
+
+function applyPermanentUpgrades() {
+    // Code to apply permanent upgrades
+}
+
+function activatePowerup(powerup) {
+    // Code to activate the selected powerup
+}
